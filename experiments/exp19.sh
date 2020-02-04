be3df3617a0f73b877a408701991a728a497de44
AnonymousFool <qsw66793@uga.edu>
2020-02-03 20:16:17 -0500

    Fixed directory structure for experiment results

--- experiments/exp19.sh
+++ experiments/exp19.sh
@@ -2,9 +2,9 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python ../Driver.py --save runs/exp19/lr1e-3.h5 --log runs/exp19/lr1e-3 --lr 1e-3 --epochs 30 --source ../../local_data/clipcropped
-python ../Driver.py --save runs/exp19/lr1e-4.h5 --log runs/exp19/lr1e-4 --lr 1e-4 --epochs 30 --source ../../local_data/clipcropped
-python ../Driver.py --save runs/exp19/lr1e-5.h5 --log runs/exp19/lr1e-5 --lr 1e-5 --epochs 30 --source ../../local_data/clipcropped
-python ../Driver.py --save runs/exp19/lr1e-6.h5 --log runs/exp19/lr1e-6 --lr 1e-6 --epochs 30 --source ../../local_data/clipcropped
-python ../Driver.py --save runs/exp19/lr1e-7.h5 --log runs/exp19/lr1e-7 --lr 1e-7 --epochs 30 --source ../../local_data/clipcropped
-python ../Driver.py --save runs/exp19/lr1e-8.h5 --log runs/exp19/lr1e-8 --lr 1e-8 --epochs 30 --source ../../local_data/clipcropped
+python ../Driver.py --save results/exp19/lr1e-3.h5 --log results/exp19/lr1e-3 --lr 1e-3 --epochs 30 --source ../../local_data/clipcropped
+python ../Driver.py --save results/exp19/lr1e-4.h5 --log results/exp19/lr1e-4 --lr 1e-4 --epochs 30 --source ../../local_data/clipcropped
+python ../Driver.py --save results/exp19/lr1e-5.h5 --log results/exp19/lr1e-5 --lr 1e-5 --epochs 30 --source ../../local_data/clipcropped
+python ../Driver.py --save results/exp19/lr1e-6.h5 --log results/exp19/lr1e-6 --lr 1e-6 --epochs 30 --source ../../local_data/clipcropped
+python ../Driver.py --save results/exp19/lr1e-7.h5 --log results/exp19/lr1e-7 --lr 1e-7 --epochs 30 --source ../../local_data/clipcropped
+python ../Driver.py --save results/exp19/lr1e-8.h5 --log results/exp19/lr1e-8 --lr 1e-8 --epochs 30 --source ../../local_data/clipcropped
