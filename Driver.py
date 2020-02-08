f31679daad697fd240ba4081f018cb8c72d79b97
Meekail Zain <zainmeekail@gmail.com>
2020-02-08 00:24:47 -0500

    Changed attr. pushthrough method

--- Driver.py
+++ Driver.py
@@ -175,9 +175,8 @@ def train(epoch):
         optimizer.zero_grad()
         recon_batch, mu, logvar, z = model(data)
         if args.distributed:
-            pseudos=model.module.pseudoGen.forward(model.module.idle_input).view(-1,1,args.input_length,args.input_length).to(device)
-        else:
-            pseudos=model.pseudoGen.forward(model.idle_input).view(-1,1,args.input_length,args.input_length).to(device)
+            MODEL=model.module
+        pseudos=model.pseudoGen.forward(model.idle_input).view(-1,1,args.input_length,args.input_length).to(device)
         recon_pseudos, p_mu, p_logvar, p_z=model(pseudos)
         loss = model.loss_function(recon_batch, data, mu, logvar, z,pseudos,recon_pseudos, p_mu, p_logvar, p_z)
         with amp.scale_loss(loss, optimizer) as scaled_loss:
@@ -210,9 +209,8 @@ def test(epoch, max, startTime):
     global stopEarly
     zTensor = torch.empty(0,args.lsdim).to(device)
     if args.distributed:
-            pseudos=model.module.pseudoGen.forward(model.module.idle_input).view(-1,1,args.input_length,args.input_length).to(device)
-    else:
-        pseudos=model.pseudoGen.forward(model.idle_input).view(-1,1,args.input_length,args.input_length).to(device)             
+            MODEL=model.module
+    pseudos=model.pseudoGen.forward(model.idle_input).view(-1,1,args.input_length,args.input_length).to(device)             
     recon_pseudos, p_mu, p_logvar, p_z=model(pseudos)
     with torch.no_grad():
         for i, data in enumerate(test_loader):
