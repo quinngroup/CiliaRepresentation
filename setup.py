ea4e73df10aa38dcc235716c77bd775a76bdd802
Meekail Zain <zainmeekail@gmail.com>
2020-02-11 12:10:15 -0500

    Updated directory structure and set up as a package

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="VTP_Zain", # Replace with your own username
+    version="0.0.1",
+    author="Meekail Zain",
+    author_email="zainmeekail@gmail.com",
+    description="An alpha build of QRG VTP pipeline",
+    url="https://github.com/quinngroup/CiliaRepresentation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
