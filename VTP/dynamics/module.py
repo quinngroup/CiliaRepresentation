b0d37d9802ea05a7498cc5d0ec25f182c5ef2f7a
alexeldridge <55260019+alexeldridge@users.noreply.github.com>
2020-02-17 18:43:04 -0500

    added ENCODERS and DECODERS dicts

--- VTP/dynamics/module.py
+++ VTP/dynamics/module.py
@@ -3,10 +3,28 @@ from torch import nn
 import torch.nn.functional as f
 from torchsummary import summary
 
+from VTP.dynamics.encoders.feedforward import FeedForward
+from VTP.dynamics.decoders.hidden_state import HiddenState
+from VTP.dynamics.decoders.neural_integrator import NeuralIntegrator
+from VTP.dynamics.decoders.recurrent import Recurrent
+from VTP.dynamics.decoders.sequential import Sequential
+
+ENCODERS = {
+    "feed forward": FeedForward
+}
+
+DECODERS = {
+    "hidden state": HiddenState,
+    "neural integrator": NeuralIntegrator,
+    "recurrent": Recurrent,
+    "sequential": Sequential
+}
 
 class VTP_D(nn.Module):
-    def __init__(self):
+    def __init__(self, encoder_type="feedforward", decoder_type="hidden state"):
         super(VTP_D, self).__init__()
+        self.encoder = ENCODERS[encoder_type]()
+        self.decoder = DECODERS[decoder_type]()
      
     # sample using mu and logvar
     def sample_from(self, mu, logvar):
@@ -14,19 +32,10 @@ class VTP_D(nn.Module):
         std_dev = torch.exp(logvar / 2)
         return mu + eps * std_dev
            
-    def forward(self, p, encoder_type="feedforward", decoder_type="sequential"):
-        if encoder_type == "feedforward":
-            return 
-        mu, logvar = self.encoder(inp)
+    def forward(self, p):
+        mu, logvar = self.encoder(p)
         w = self.sample_from(mu, logvar)
-        if decoder_type == "sequential":
-            return self.decoder_seq(w)
-        elif decoder_type == "hidden state":
-            return self.decoder_hid(w)
-        elif decoder_type == "recurrent":
-            return self.decoder_rec(w)
-        elif decoder_type == "neural integrator":
-            return self.decoder_ode(w)
+        return self.decoder(w)
 
 #model = VTP_D()
 #summary(model, (20, 2))
