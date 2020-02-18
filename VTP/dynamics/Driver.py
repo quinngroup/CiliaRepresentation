fedc7e4dc2e6238247210cc98ebe4ab0d7bece9f
alexeldridge <55260019+alexeldridge@users.noreply.github.com>
2020-02-17 19:12:29 -0500

    created empty model, train, test functions

--- VTP/dynamics/Driver.py
+++ VTP/dynamics/Driver.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from VTP.dynamics.module import VTP_D
 
 #Parses arguments passed from command line
 parser = ArgumentParser(description='DynamicsDriver')
@@ -6,3 +7,12 @@ parser.add_argument('--encoder', type=str, default=None,
     help='choose the desired encoder architecture')
 parser.add_argument('--decoder', type=str, default=None, 
     help='choose the desired decoder architecture')
+args = parser.parse_args()
+
+model = VTP_D()
+
+def train():
+    pass
+
+def test():
+    pass
