9e15939b676e06b0897559f167117003cec3e817
alexeldridge <55260019+alexeldridge@users.noreply.github.com>
2020-02-18 19:46:01 -0500

    implemented baseline train function

--- VTP/dynamics/Driver.py
+++ VTP/dynamics/Driver.py
@@ -9,10 +9,21 @@ parser.add_argument('--decoder', type=str, default=None,
     help='choose the desired decoder architecture')
 args = parser.parse_args()
 
-model = VTP_D()
+model = VTP_D(encoder=args.encoder, decoder=args.decoder)
+
+train_loader = None
+test_loader = None
+optimizer = None
 
 def train():
-    pass
+    model.train()
+    train_loss = 0
+    for batch_idx, data in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = model.loss_function(output, data)
+        train_loss += loss.item()
+        optimizer.step()
 
 def test():
-    pass
+    model.eval()
