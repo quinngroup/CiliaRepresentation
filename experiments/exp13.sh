3da44b89295a544f8a1660116929c71d107b6a7b
Bella Humphrey <bellahumphrey@Bellas-MacBook-Air-2.local>
2020-02-24 20:13:21 -0500

    Renamed experiment 14 to the proper name

--- experiments/exp13.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-
-python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --save ../results/exp13/NVP.h5 --log ../results/exp13/NVP --epochs 30 --source ../../local_data/patches --log_image 2
-python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --model 'nvp1' --save ../results/exp13/NVP_4.h5 --log ../results/exp13/NVP_4 --epochs 30 --source ../../local_data/patches --log_image 2
