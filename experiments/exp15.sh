a53d4c7784baa3cc2251d234093a482091e525a8
Meekail Zain <zainmeekail@gmail.com>
2020-02-28 20:53:53 -0500

    Copied NVP_4 trial history and modified logdir for exp15.sh

--- experiments/exp15.sh
+++ experiments/exp15.sh
@@ -2,4 +2,4 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --model nvp5 --save ../results/exp15/t1.h5 --log ../results/exp15/ --epochs 90 --source ../../local_data/patches --log_image 2 --lsdim 500 --batch_size 22 --roll 4
+python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --model nvp5 --save ../results/exp15/NVP_5.h5 --log ../results/exp15/NVP_5/ --epochs 90 --source ../../local_data/patches --log_image 2 --lsdim 500 --batch_size 22 --roll 4
