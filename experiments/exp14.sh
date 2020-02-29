d8664df5e402497761fe441ec93c7ae8e1a6915a
Meekail Zain <zainmeekail@gmail.com>
2020-02-28 20:31:47 -0500

    Added results of exp14

--- experiments/exp14.sh
+++ experiments/exp14.sh
@@ -2,4 +2,4 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --model nvp4 --save ../results/exp14/NVP_4.h5 --log ../results/exp14/ --epochs 90 --source ../../local_data/patches --log_image 2 --lsdim 500 --batch_size 28
+python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --model nvp4 --save ../results/exp14/NVP_4_lr.h5 --log ../results/exp14/lr --epochs 90 --source ../../local_data/patches --log_image 2 --lsdim 500 --batch_size 22 --roll 4 --lr 1e-4
