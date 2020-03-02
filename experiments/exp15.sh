dd1f2e9b87c949bac5e89d9bf2b23eeae25f3ef0
Meekail Zain <zainmeekail@gmail.com>
2020-03-01 22:10:14 -0500

    Results of exp15

--- experiments/exp15.sh
+++ experiments/exp15.sh
@@ -2,4 +2,4 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --model nvp5 --save ../results/exp15/NVP_5.h5 --log ../results/exp15/NVP_5/ --epochs 90 --source ../../local_data/patches --log_image 2 --lsdim 500 --batch_size 22 --roll 4
+python -m torch.distributed.launch --nproc_per_node=2 ../VTP/appearance/Driver.py --model nvp5 --save ../results/exp15/NVP_5.h5 --log ../results/exp15/NVP_5/ --epochs 90 --source ../../local_data/patches --log_image 2 --lsdim 500 --batch_size 22 --roll 4 --lr 1e-4
