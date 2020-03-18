516f2a852aeb02b5d3494050c910dbaf4e5a6795
nssafir <nssafir@gmail.com>
2020-03-18 17:50:04 -0400

    Made very small changes to data preprocessing file

--- VTP/utils/makeDataLoader.py
+++ VTP/utils/makeDataLoader.py
@@ -1,9 +1,12 @@
-# convert to pandas dataframe
+from sktime.utils.load_data import load_from_tsfile_to_dataframe
+import pandas as pd
+import numpy as np
+
+# convert to pandas dataframe and then numpy array
 
 filePath = ""
 
-from sktime.utils.load_data import load_from_tsfile_to_dataframe
-train_x, train_y = load_from_tsfile_to_dataframe(filePath) 
-test_x, test_y = load_from_tsfile_to_dataframe(filePath)
+train_x, train_y = load_from_tsfile_to_dataframe(filePath).to_numpy()
+test_x, test_y = load_from_tsfile_to_dataframe(filePath).to_numpy()
 
 
