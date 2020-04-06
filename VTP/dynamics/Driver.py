27d82bdacbb5c3adebc18f6626e4999b2aba1c27
nssafir <nssafir@gmail.com>
2020-04-06 17:40:38 -0500

    Updated Driver.py and made Driver2.py. Will probably use Driver2.py in the furuter (currently just a copy of the driver file for appearance module) bc it runs on distributed already. See new issues in driver milestone

--- VTP/dynamics/Driver.py
+++ VTP/dynamics/Driver.py
@@ -11,13 +11,166 @@ parser.add_argument('--trainsource', type=str, default=None,
     help='directory containing source training files')
 parser.add_argument('--testsource', type=str, default=None,
     help='directory containing source testing files')
+parser.add_argument('--optimizer', type=str, default=None, #ADD DEFAULT OPTIMIZER
+    help='directory containing source testing files')
+parser.add_argument('--no_cuda', action='store_true', default=False,
+    help='enables CUDA training')
 args = parser.parse_args()
 
 model = VTP_D(encoder=args.encoder, decoder=args.decoder)
 
-train_loader = None
-test_loader = None
-optimizer = None
+# DISTRIBUTED PROGRAMMING STUFF BELOW
+
+#Runs model on GPU by default, but on CPU if GPU not available
+cuda = not args.no_cuda and torch.cuda.is_available()
+if cuda:
+    device = 'cuda'
+    kwargs = {'num_workers': 1, 'pin_memory': True}
+    #Tests CUDA compatibility before instantiating dataset
+    with torch.cuda.device(0):
+        torch.tensor([1.]).cuda()
+else:
+    device = 'cpu'
+    kwargs = {}
+
+#Determine whether the process is being run as distributed
+args.distributed = False
+if 'WORLD_SIZE' in os.environ:
+    worldCount=int(os.environ['WORLD_SIZE'])
+    args.distributed = worldCount > 1
+
+if args.distributed:
+    # FOR DISTRIBUTED:  Set the device according to local_rank.
+    torch.cuda.set_device(args.local_rank)
+
+    # FOR DISTRIBUTED:  Initialize the backend.  torch.distributed.launch will provide
+    # environment variables, and requires that you use init_method=`env://`.
+    torch.distributed.init_process_group(backend='nccl',
+                                         init_method='env://')
+
+torch.backends.cudnn.benchmark = True
+
+#Manually sets random seed if requested
+if args.seed:
+    torch.manual_seed(args.seed)
+
+writer=None
+if(args.log!='!' and args.local_rank==0):
+    if(args.log=='$'):
+        writer = SummaryWriter()
+    else:
+        writer = SummaryWriter(log_dir=args.log)
+
+arguments=[args.input_length, args.lsdim, args.pseudos, args.beta, args.gamma, device, args.logvar_bound]
+if args.model=='nvp':
+    model = NVP(*arguments)
+elif args.model=='nvp1':
+    model = NVP_1.NVP_1(*arguments)
+elif args.model=='nvp4':
+    model = NVP_4.NVP_4(*arguments)
+
+model.cuda()
+optimizer = torch.optim.Adam([{'params': model.vae.parameters()},
+                        {'params': model.pseudoGen.parameters(), 'lr': args.plr}],
+                        lr=args.lr, weight_decay=args.reg2)
+
+#Amp optional fp optimization
+model, optimizer = amp.initialize(model, optimizer, opt_level="O0")
+
+if args.distributed:
+    # FOR DISTRIBUTED:  After amp.initialize, wrap the model with
+    model = torch.nn.parallel.DistributedDataParallel(model,
+                                                    device_ids=[args.local_rank],
+                                                    output_device=args.local_rank)
+
+#Construct datasets
+if args.dataset == 'od':
+    data = Datasets.overlapDataset(args.source, args.clip_length)
+elif args.dataset == 'nd':
+    data = Datasets.nonOverlapDataset(args.source, args.clip_length)
+elif args.dataset == 'frd':
+    data = Datasets.frameDataset(args.source, transforms.ToTensor())
+elif args.dataset == 'nwd':
+    data = Datasets.nonOverlapWindowDataset(args.source, args.input_height, args.input_length, transforms.ToTensor())
+elif args.dataset == 'owd':
+    data = Datasets.overlapWindowDataset(args.source, args.input_height, args.input_length, transforms.ToTensor())
+elif args.dataset == 'ncd':
+    data = Datasets.nonOverlapClipDataset(args.source, args.clip_length, args.input_height, args.input_length)
+elif args.dataset == 'ocd':
+    data = Datasets.overlapClipDataset(args.source, args.clip_length, args.input_height, args.input_length)
+else:
+    data = None
+
+testSize = ceil(args.test_split * len(data))
+trainSize = len(data) - testSize
+trainSet, testSet = random_split(data, [trainSize, testSize])
+train_sampler=None
+test_sampler=None
+
+if args.distributed:
+    train_sampler = torch.utils.data.distributed.DistributedSampler(trainSet)
+    test_sampler = torch.utils.data.distributed.DistributedSampler(testSet)
+
+train_loader = DataLoader(
+    trainSet,
+    batch_size=args.batch_size,
+    shuffle=(train_sampler is None),
+    num_workers=args.workers,
+    pin_memory=True,
+    sampler=train_sampler)
+
+test_loader = DataLoader(
+    testSet,
+    batch_size=args.batch_size,
+    shuffle=False,
+    num_workers=args.workers,
+    pin_memory=True,
+    sampler=test_sampler)
+
+
+imagePace=None
+if args.log_image>0 and args.log!='!':
+    imagePace=len(train_loader.dataset)//(args.batch_size*args.log_image)
+
+
+stopEarly = False
+failedEpochs=0
+lastLoss = 0
+
+scheduler=None
+if(args.schedule>0):
+            scheduler=lr_scheduler.ReduceLROnPlateau(optimizer, min_lr = args.min_lr, verbose=True, patience=args.schedule)
+
+
+scale=1
+if args.distributed:
+    scale=2
+
+def printLoss(phase, loss, epoch=None, batch_idx=None, data_length=None, genLoss=None):
+    if phase == 'train':
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tGenLoss: {:.6f}'.format(
+            epoch, scale*batch_idx * data_length, len(train_loader.dataset),
+            100. * batch_idx / len(train_loader),
+            loss.item() / data_length,
+            genLoss))
+    elif phase == 'average':
+        print('====> Epoch: {} Average loss (main gpu): {:.4f}'.format(
+            epoch, scale*loss / len(train_loader.dataset)))
+    elif phase=='test':
+        print('====> Test set loss: {:.4f}'.format(loss))
+        print('====> Generation loss: {:.4f}'.format(genLoss))
+    else:
+        print('Loss printing error')
+    
+    
+    
+
+# NEED TO FIGURE OUT HOW TO GO FROM INPUT STRING TO FILE
+train_loader = args.trainsource
+test_loader = args.testsource
+
+# NEED TO FIGURE OUT HOW TO LOAD IN OPTIMIZER
+optimizer = args.optimizer
 
 def train():
     model.train()
---
+++ VTP/dynamics/Driver2.py
@@ -0,0 +1,418 @@
+from argparse import ArgumentParser
+from math import ceil
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+from NVP import NVP
+from test_builds import NVP_1, NVP_4
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import StandardScaler
+import time
+import torch
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader, random_split
+from torch.utils.tensorboard import SummaryWriter
+from torchsummary import summary
+from torchvision import transforms
+from VTP.utils import Datasets
+import apex
+from apex import amp
+import os
+import numpy as np
+'''
+Appearance Module Driver
+@author Quinn Wyner
+'''
+
+startTime = time.time()
+
+#Parses arguments passed from command line
+parser = ArgumentParser(description='DynamicsDriver')
+parser.add_argument('--batch_size', type=int, default=75, metavar='b',
+                    help='number of elements per minibatch')
+parser.add_argument('--beta', type=float, default=1.0, metavar='b',
+                    help='sets the value of beta for a beta-vae implementation')
+parser.add_argument('--dataset', type=str, default='nwd', metavar='D',
+                    help='dataset selection for training and testing')
+parser.add_argument('--dbscan', action='store_true', default= False,
+                    help='to run dbscan clustering')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--failCount', type=str, default='r', metavar='fc',
+                    help='determines how to reset early-stopping failed epoch counter. Options are \'r\' for reset and \'c\' for cumulative')
+parser.add_argument('--gamma', type = float, default=.05, metavar='g',
+                    help='Pseudo-loss weight')
+parser.add_argument('--graph', action='store_true', default= False,
+                    help='flag to determine whether or not to generate a tensorboard graph')
+parser.add_argument('--input_height', type=int, default=128, metavar='ih',
+                    help='height of each patch')
+parser.add_argument('--input_length', type=int, default=128, metavar='il',
+                    help='length of each patch')
+parser.add_argument('--load', type=str, default='', metavar='l',
+                    help='loads the weights from a given filepath')
+parser.add_argument("--local_rank", type=int, default=0,
+                    help='parameter for distributed training provided by launch script')
+parser.add_argument('--log', type=str, default='!', metavar='lg',
+                    help='flag to determine whether to use tensorboard for logging. Default \'!\' is read to mean no logging')
+parser.add_argument('--log_interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--log_image', type=int, default=0, metavar='N',
+                    help='how many images to record to tensorboard per epoch')
+parser.add_argument('--logvar_bound', type=float, default=-1.0, metavar='lb',
+                    help='Lower bound on logvar (default: -1.0)')
+parser.add_argument('--lr', type = float, default=1e-5, metavar='lr',
+                    help='learning rate')
+parser.add_argument('--lsdim', type = int, default=10, metavar='ld',
+                    help='sets the number of dimensions in the latent space. should be >1. If  <3, will generate graphical representation of latent without TSNE projection')
+                    #current implementation may not be optimal for dims above 4
+parse.add_argument('--min_lr', type = float, default=0, metavar='minlr',
+                    help='sets the lower bound on the learning rate of all param groups')
+parser.add_argument('--model', type=str, default='nvp',
+                    help='determines which model to use')
+parser.add_argument('--no_cuda', action='store_true', default=False,
+                    help='enables CUDA training')
+parser.add_argument('--noEarlyStop', action='store_true', default=False,
+                    help='disables early stopping')
+parser.add_argument('--patience', type = int, default = 10, metavar='pat',
+                    help='patience value for early stopping')
+parser.add_argument('--plr', type = float, default=4e-6, metavar='plr',
+                    help='pseudoinput learning rate')
+parser.add_argument('--print_pseudos', type = int, default=0, metavar='pp',
+                    help='Plot pseudos. Controls the number of pseudo inputs to be displayed')
+parser.add_argument('--pseudos', type=int, default=10, metavar='p',
+                    help='Number of pseudo-inputs (default: 10)')
+parser.add_argument('--reg2', type = float, default=5e-4, metavar='rg2',
+                    help='coefficient for L2 weight decay')
+parser.add_argument('--repeat', action='store_true', default=False,
+                    help='determines whether to enact further training after loading weights')
+parser.add_argument('--roll', type = int, default = 1, metavar='r',
+                    help='over how many batches to accumulate gradients')
+parser.add_argument('--save', type=str, default='', metavar='s',
+                    help='saves the weights to a given filepath')
+parser.add_argument('--schedule', type = int, default=-1, metavar='sp',
+                    help='use learning rate scheduler on loss stagnation with input patience')
+parser.add_argument('--seed', type=int, default=None, metavar='s',
+                    help='manual random seed (default: None)')
+parser.add_argument('--source', type=str, default='..\data', metavar='S',
+                    help='directory containing source files')
+parse.add_argument('--start_lr_schedule', type = int, default=1, metavar='stlrsp'
+                    help='what epoch at which to start the learning rate scheduling')
+parser.add_argument('--test_split', type=float, default=.2, metavar='ts',
+                    help='portion of data reserved for testing')
+parser.add_argument('--tolerance', type = float, default=.1, metavar='tol',
+                    help='tolerance value for early stopping')
+parser.add_argument('--tsne', action='store_true', default=False,
+                    help='Uses TSNE projection instead of UMAP.')
+parser.add_argument('--vis', action='store_true', default= False,
+                    help='flag to determine whether or not to automatically visalize latent space')
+parser.add_argument('--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+
+args = parser.parse_args()
+
+
+
+
+
+
+#Runs model on GPU by default, but on CPU if GPU not available
+cuda = not args.no_cuda and torch.cuda.is_available()
+if cuda:
+    device = 'cuda'
+    kwargs = {'num_workers': 1, 'pin_memory': True}
+    #Tests CUDA compatibility before instantiating dataset
+    with torch.cuda.device(0):
+        torch.tensor([1.]).cuda()
+else:
+    device = 'cpu'
+    kwargs = {}
+
+#Determine whether the process is being run as distributed
+args.distributed = False
+if 'WORLD_SIZE' in os.environ:
+    worldCount=int(os.environ['WORLD_SIZE'])
+    args.distributed = worldCount > 1
+
+if args.distributed:
+    # FOR DISTRIBUTED:  Set the device according to local_rank.
+    torch.cuda.set_device(args.local_rank)
+
+    # FOR DISTRIBUTED:  Initialize the backend.  torch.distributed.launch will provide
+    # environment variables, and requires that you use init_method=`env://`.
+    torch.distributed.init_process_group(backend='nccl',
+                                         init_method='env://')
+
+torch.backends.cudnn.benchmark = True
+
+#Manually sets random seed if requested
+if args.seed:
+    torch.manual_seed(args.seed)
+
+writer=None
+if(args.log!='!' and args.local_rank==0):
+    if(args.log=='$'):
+        writer = SummaryWriter()
+    else:
+        writer = SummaryWriter(log_dir=args.log)
+
+arguments=[args.input_length, args.lsdim, args.pseudos, args.beta, args.gamma, device, args.logvar_bound]
+if args.model=='nvp':
+    model = NVP(*arguments)
+elif args.model=='nvp1':
+    model = NVP_1.NVP_1(*arguments)
+elif args.model=='nvp4':
+    model = NVP_4.NVP_4(*arguments)
+
+model.cuda()
+optimizer = torch.optim.Adam([{'params': model.vae.parameters()},
+                        {'params': model.pseudoGen.parameters(), 'lr': args.plr}],
+                        lr=args.lr, weight_decay=args.reg2)
+
+#Amp optional fp optimization
+model, optimizer = amp.initialize(model, optimizer, opt_level="O0")
+
+if args.distributed:
+    # FOR DISTRIBUTED:  After amp.initialize, wrap the model with
+    model = torch.nn.parallel.DistributedDataParallel(model,
+                                                    device_ids=[args.local_rank],
+                                                    output_device=args.local_rank)
+
+#Construct datasets
+if args.dataset == 'od':
+    data = Datasets.overlapDataset(args.source, args.clip_length)
+elif args.dataset == 'nd':
+    data = Datasets.nonOverlapDataset(args.source, args.clip_length)
+elif args.dataset == 'frd':
+    data = Datasets.frameDataset(args.source, transforms.ToTensor())
+elif args.dataset == 'nwd':
+    data = Datasets.nonOverlapWindowDataset(args.source, args.input_height, args.input_length, transforms.ToTensor())
+elif args.dataset == 'owd':
+    data = Datasets.overlapWindowDataset(args.source, args.input_height, args.input_length, transforms.ToTensor())
+elif args.dataset == 'ncd':
+    data = Datasets.nonOverlapClipDataset(args.source, args.clip_length, args.input_height, args.input_length)
+elif args.dataset == 'ocd':
+    data = Datasets.overlapClipDataset(args.source, args.clip_length, args.input_height, args.input_length)
+else:
+    data = None
+
+testSize = ceil(args.test_split * len(data))
+trainSize = len(data) - testSize
+trainSet, testSet = random_split(data, [trainSize, testSize])
+train_sampler=None
+test_sampler=None
+
+if args.distributed:
+    train_sampler = torch.utils.data.distributed.DistributedSampler(trainSet)
+    test_sampler = torch.utils.data.distributed.DistributedSampler(testSet)
+
+train_loader = DataLoader(
+    trainSet,
+    batch_size=args.batch_size,
+    shuffle=(train_sampler is None),
+    num_workers=args.workers,
+    pin_memory=True,
+    sampler=train_sampler)
+
+test_loader = DataLoader(
+    testSet,
+    batch_size=args.batch_size,
+    shuffle=False,
+    num_workers=args.workers,
+    pin_memory=True,
+    sampler=test_sampler)
+
+
+imagePace=None
+if args.log_image>0 and args.log!='!':
+    imagePace=len(train_loader.dataset)//(args.batch_size*args.log_image)
+
+
+stopEarly = False
+failedEpochs=0
+lastLoss = 0
+
+scheduler=None
+if(args.schedule>0):
+            scheduler=lr_scheduler.ReduceLROnPlateau(optimizer, min_lr = args.min_lr, verbose=True, patience=args.schedule)
+
+
+scale=1
+if args.distributed:
+    scale=2
+
+def printLoss(phase, loss, epoch=None, batch_idx=None, data_length=None, genLoss=None):
+    if phase == 'train':
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tGenLoss: {:.6f}'.format(
+            epoch, scale*batch_idx * data_length, len(train_loader.dataset),
+            100. * batch_idx / len(train_loader),
+            loss.item() / data_length,
+            genLoss))
+    elif phase == 'average':
+        print('====> Epoch: {} Average loss (main gpu): {:.4f}'.format(
+            epoch, scale*loss / len(train_loader.dataset)))
+    elif phase=='test':
+        print('====> Test set loss: {:.4f}'.format(loss))
+        print('====> Generation loss: {:.4f}'.format(genLoss))
+    else:
+        print('Loss printing error')
+
+def train(epoch):
+    before, after=None,None
+    if args.distributed:
+        train_sampler.set_epoch(epoch)
+    model.train()
+    train_loss = 0
+    roll_loss = 0
+    for batch_idx, data in enumerate(train_loader):
+        data = data.to(device)
+        recon_batch, mu, logvar, z = model(data)
+        if imagePace is not None and args.local_rank==0 and batch_idx % imagePace == 0:
+            before=data[0].cpu().detach().numpy()
+            after=recon_batch[0].cpu().detach().numpy()
+            image_step=(epoch-1)*args.log_image+(batch_idx//imagePace)
+            writer.add_images('reconstructions/'+str(image_step), np.concatenate((before[np.newaxis],after[np.newaxis])), image_step,dataformats='NCHW')
+
+
+        #For model module access, must reference model.module
+        #if distributed to get through the distributed wrapper class
+        if args.distributed:
+            MODEL=model.module
+        else:
+            MODEL=model
+
+        pseudos=MODEL.pseudoGen.forward(MODEL.idle_input).view(-1,1,args.input_length,args.input_length).to(device)
+        recon_pseudos, p_mu, p_logvar, p_z=model(pseudos)
+        loss = MODEL.loss_function(recon_batch, data, mu, logvar, z,pseudos,recon_pseudos, p_mu, p_logvar, p_z)
+        with amp.scale_loss(loss, optimizer) as scaled_loss:
+            scaled_loss.backward()
+        train_loss += loss.item()
+        roll_loss += loss.item()
+        genLoss = MODEL.loss_function(recon_batch, data, mu, logvar, z, pseudos, recon_pseudos, p_mu, p_logvar, p_z, gamma=0).item() / len(data)
+
+        if (batch_idx+1)%args.roll == 0:
+            # every [args.roll] iterations of batches [args.batch_size]
+            # for an effective batch-size of [args.roll]*[args.batch_size]
+            if(args.log!='!' and args.local_rank==0):
+                step=((epoch-1)*len(train_loader)+batch_idx)//args.roll
+                per_item_loss=roll_loss/(len(data)*args.roll)
+                writer.add_scalar('item_loss',per_item_loss,global_step=step)
+            optimizer.step()
+            optimizer.zero_grad()
+            roll_loss=0
+
+        if batch_idx % args.log_interval == 0 and args.local_rank==0:
+            printLoss('train', loss, epoch, batch_idx, len(data), genLoss)
+
+    '''Note in the "average loss" section we multiply by the constant scale
+    since if the model is run in distributed mode, each singular gpu will only
+    process *half* the data but would otherwise normalize by the length of the entire dataset
+    '''
+    if args.local_rank==0:
+        printLoss('average', train_loss, epoch)
+    if(args.schedule>0):
+        if(epochs>=start_lr_schedule):
+            scheduler.step(scheduler.step(scale*train_loss / len(train_loader.dataset)))
+
+def test(epoch, max, startTime):
+    model.eval()
+    test_loss = 0
+    gen_loss = 0
+    global lastLoss
+    global failedEpochs
+    global stopEarly
+    zTensor = torch.empty(0,args.lsdim).to(device)
+    if args.distributed:
+        MODEL=model.module
+    else:
+        MODEL=model
+    pseudos=MODEL.pseudoGen.forward(MODEL.idle_input).view(-1,1,args.input_length,args.input_length).to(device)
+    recon_pseudos, p_mu, p_logvar, p_z=model(pseudos)
+    with torch.no_grad():
+        for i, data in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar, z = model(data)
+            test_loss += MODEL.loss_function(recon_batch, data, mu, logvar,z,pseudos,recon_pseudos, p_mu, p_logvar, p_z).item()
+            gen_loss += MODEL.loss_function(recon_batch, data, mu, logvar, z, pseudos, recon_pseudos, p_mu, p_logvar, p_z, gamma=0).item()
+            zTensor = torch.cat((zTensor, z), 0)
+    if (args.dbscan == True) :
+        zScaled = StandardScaler().fit_transform((torch.Tensor.cpu(zTensor).numpy())) #re-add StandardScaler().fit_transform
+        db = DBSCAN(eps= 0.7, min_samples= 3).fit(zScaled)
+        print(db.labels_)
+        labelTensor = db.labels_
+    test_loss /= len(test_loader.dataset)/scale
+    gen_loss /= len(test_loader.dataset)/scale
+    if args.local_rank==0:
+        printLoss('test', test_loss, genLoss=gen_loss)
+    if(epoch == 1):
+        lastLoss = test_loss
+    elif not args.noEarlyStop:
+        if lastLoss-test_loss < args.tolerance:
+            failedEpochs += 1
+            if failedEpochs >= args.patience:
+                stopEarly = True
+
+                epoch = max
+        elif args.failCount == 'r':
+            failedEpochs = 0
+    if(epoch == max and args.local_rank==0):
+        if(args.save != ''):
+            torch.save({
+                        'model_state_dict':model.state_dict(),
+                        'optimizer_state_dict':optimizer.state_dict()
+                        }, args.save)
+        print("--- %s seconds ---" % (time.time() - startTime))
+        cmap = colors.ListedColormap(['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabebe'])
+
+        #Handling different dimensionalities
+        if(args.vis):
+            if (args.lsdim < 3) :
+                z1 = torch.Tensor.cpu(zTensor[:, 0]).numpy()
+                z2 = torch.Tensor.cpu(zTensor[:, 1]).numpy()
+                scatterPlot = plt.scatter(z1, z2, s = 4) #Regular 2dim plot, RE-ADD CMAP = CMAP
+            elif (args.lsdim == 3) :
+                fig=plt.figure()
+                ax=fig.gca(projection='3d')
+                z1 = torch.Tensor.cpu(zTensor[:, 0]).numpy()
+                z2 = torch.Tensor.cpu(zTensor[:, 1]).numpy()
+                z3 = torch.Tensor.cpu(zTensor[:, 2]).numpy()
+                scatterPlot = ax.scatter(z1, z2, z3, s = 4) #Regular 3dim plot
+            elif args.tsne:
+                Z_embedded = TSNE(n_components=2, verbose=1).fit_transform(zTensor.cpu())
+                z1 = Z_embedded[:, 0]
+                z2 = Z_embedded[:, 1]
+                scatterPlot = plt.scatter(z1, z2, s = 4) #TSNE projection for >3dim
+            else:
+                reducer = umap.UMAP()
+                Z_embedded = reducer.fit_transform(zTensor.cpu())
+                scatterPlot = plt.scatter(Z_embedded[:, 0], Z_embedded[:, 1], s = 4)
+
+            plt.show()
+        if(args.print_pseudos>0):
+            t=min(args.print_pseudos,args.pseudos)
+            temp = model.means(model.idle_input).view(-1,args.input_length,args.input_length).detach().cpu()
+            for x in range(t):
+                plt.matshow(temp[x].numpy())
+                plt.show()
+
+if not args.distributed:
+    summary(model.cuda(),(1,args.input_length,args.input_length))
+if(args.load == ''):
+    for epoch in range(1, args.epochs + 1):
+        if(not stopEarly):
+            train(epoch)
+            test(epoch, args.epochs, startTime)
+elif args.local_rank==0:
+    checkpoint=torch.load(args.load)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+test(args.epochs, args.epochs, startTime)
+if(args.repeat):
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        test(epoch, args.epochs, startTime)
+
+if(args.log!='!' and args.local_rank==0):
+    if args.graph:
+        dummy = torch.autograd.Variable(torch.Tensor(1,1,128,128), requires_grad=True).to(device)
+        writer.add_graph(model.eval(),dummy,verbose=True)
+    writer.close()
